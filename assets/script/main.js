1a2d297807d9f0a0556de9dee1bfbf9760761b0c
t210mha <t210mha@gmail.com>
2024-06-18 01:19:05 +0900

    ヘッダーPC表示作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,5 @@
 // swiper
 const swiper = new Swiper('.swiper', {
-    // loop: true, // スライドをループする
     slidesPerView: 1.25,
     loopedSlides: 3, // 3つのスライドをループに含める
     initialSlide: 1, // 初期表示スライドを2番目に設定
@@ -13,5 +12,13 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
         el: '.swiper-scrollbar',
         hide: true, // スクロールバーを非表示にする
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            slidesPerGroup: 1,
+            spaceBetween: 15,
+            initialSlide: 1
+        },
     },
 });
